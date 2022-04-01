b0e7f9d780cca178e41bfd0a32684a55d040b3a5
Сергеев Евгений <sevg3ny@gmail.com>
2022-04-01 19:43:30 +0300

    Решена задача 29: реализована функция, которая проверяет, просто ли число (проверка вынесенеа в отдельную функцию).

--- task_29/sayPrimeOrNot.php
+++ task_29/sayPrimeOrNot.php
@@ -2,11 +2,15 @@
 
 declare(strict_types=1);
 
-function sayPrimeOrNot(int $num) : string {
+function sayPrimeOrNot(int $num) : void {
     for ($i = 2; $i < $num; $i++)
-        if ($num % $i == 0) return 'no';
-    return 'yes';
+        if ($num % $i == 0){ printResult(false); return; }
+    printResult(true);
 }
 
-echo sayPrimeOrNot(131) . "\n"; // => yes
-echo sayPrimeOrNot(4); // => no
+function printResult(bool $prime) : void {
+    echo $prime? "yes\n" : "no\n";
+}
+
+sayPrimeOrNot(8);
+sayPrimeOrNot(131);
